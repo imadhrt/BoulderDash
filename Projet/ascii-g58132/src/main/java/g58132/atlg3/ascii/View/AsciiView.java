34fec6dae4df81c0e6b4e22263f319a76f9fb731
58132@etu.he2b.be <58132@etu.he2b.be>
2022-10-06 04:33:24 +0200

    fin td3 mais petit problème encore à regler dans l'affichage du drawing

--- Projet/ascii-g58132/src/main/java/g58132/atlg3/ascii/View/AsciiView.java
+++ Projet/ascii-g58132/src/main/java/g58132/atlg3/ascii/View/AsciiView.java
@@ -1,6 +1,62 @@
 package g58132.atlg3.ascii.View;
 
+import g58132.atlg3.ascii.Model.Drawing;
+import g58132.atlg3.ascii.Model.Point;
+
+/**
+ * displayHelp(),display(Drawing d),asAsci() pas dans asciiPaint....
+ **/
 public class AsciiView {
-    /** displayHelp(),display(Drawing d),asAsci() pas dans asciiPaint....**/
+private Drawing draws;
+    public AsciiView(Drawing draws) {
+        this.draws=draws;
+    }
+
+    /**
+     * Display help
+     */
+
+    public void displayHelp() {
+        System.out.println("The command entered is not valid!!!" +
+                         "the commands used are ADD, SHOW,HELP, EXIT and with their parameters");
+    }
+
+
+    /**
+     * Display of the shape
+     *
+     * @param drawing is a drawing
+     */
+    public void display(Drawing drawing) {
+        for (int i = 0; i < drawing.getHeight(); i++) {
+            for (int j = 0; j < drawing.getWidth(); j++) {
+                Point pointShape = new Point(i, j);
+                if (drawing.getShapeAt(pointShape) != null) {
+                    System.out.print(drawing.getShapeAt(pointShape).getColor());
+                }
+
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    /**
+     * Display start of game
+     */
+    public void displayStartGame() {
+        System.out.println("Welcome to the game of ASCIIPAINT");
+    }
+
+    /**
+     * Display end of game
+     */
+    public void displayEnd() {
+        System.out.println("*****End of game*****");
+    }
+
+
 }
 
--- Projet/ascii-g58132/src/main/java/g58132/atlg3/ascii/controller/Application.java
+++ Projet/ascii-g58132/src/main/java/g58132/atlg3/ascii/controller/Application.java
@@ -1,12 +1,87 @@
 package g58132.atlg3.ascii.controller;
 
+import g58132.atlg3.ascii.Model.AsciiPaint;
+import g58132.atlg3.ascii.Model.Drawing;
+import g58132.atlg3.ascii.View.AsciiView;
+
+import java.util.Scanner;
+
 public class Application {
+
+private final AsciiPaint paint;
+private final AsciiView view;
+
+    public Application(AsciiPaint paint, AsciiView view) {
+        this.paint = paint;
+        this.view = view;
+    }
 
 
     public static void main(String[] args) {
-       /**faire une boucle tant que le joueur veut continuer,je demande quel il veut (add show,help) si il veut
-        * arreter console exit **/
+        /** Faire une boucle tant que le joueur veut continuer,je demande lequel il veut (add show,help) si il veut
+         * arreter console exit  **/
 
-        System.out.println("allo");   System.out.println("allo");   System.out.println("allo");   System.out.println("allo");
+         AsciiPaint game=new AsciiPaint();
+         Drawing drawing=new Drawing();
+        Application controller=new Application(game,new AsciiView(drawing));
+        controller.start();
+
+
+
+
     }
-}
+
+    /**
+     * Start the game
+     */
+    public void start() {
+        Scanner keyboard=new Scanner(System.in);
+        view.displayStartGame();
+        String command;
+        boolean isGameOver=false;
+
+
+        while(!isGameOver){
+            command=keyboard.nextLine().toLowerCase();
+            String [] tab=command.split(" ");
+
+
+             if (command.equals("exit")) {
+                isGameOver=true;
+            }
+            else if (tab.length==6&&tab[0].equals("add")&&tab[1].equals("circle")&& tab[2].matches("-?\\d+")
+           && tab[3].matches("-?\\d+") && tab[4].matches("-?\\d+") && tab[5].matches("[a-z]")){
+                paint.newCircle(Integer.parseInt(tab[2]),Integer.parseInt(tab[3]),Integer.parseInt(tab[4]),tab[5].charAt(0));
+            }
+            else if (tab.length==6&&tab[0].equals("add")&&tab[1].equals("square")&& tab[2].matches("-?\\d+")
+                    && tab[3].matches("-?\\d+") && tab[4].matches("-?\\d+") && tab[5].matches("[a-z]")) {
+                paint.newSquare(Integer.parseInt(tab[2]),Integer.parseInt(tab[3]),Integer.parseInt(tab[4]),tab[5].charAt(0));
+                
+            }
+            else if (tab.length==7&&tab[0].equals("add")&&tab[1].equals("rectangle")&& tab[2].matches("-?\\d+")
+                    && tab[3].matches("-?\\d+") && tab[4].matches("-?\\d+")&&tab[5].matches("-?\\d+") && tab[6].matches("[a-z]")) {
+                System.out.println("add rectangle");
+                paint.newRectangle(Integer.parseInt(tab[2]),Integer.parseInt(tab[3]),Integer.parseInt(tab[4]),Integer.parseInt(tab[5]),tab[6].charAt(0));
+            }
+            else if(command.equals("show")){
+                view.display(new Drawing());
+
+            }
+            else{
+                view.displayHelp();
+
+            }
+
+        }
+        view.displayEnd();
+
+
+        }
+
+
+
+
+
+
+    }
+
