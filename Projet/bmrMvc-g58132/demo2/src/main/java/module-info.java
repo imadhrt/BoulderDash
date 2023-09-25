fed0e564b9f01025d9117517f95a84c5df37f60a
imadhrt <135547612+imadhrt@users.noreply.github.com>
2023-09-25 22:19:20 +0200

    Delete Projet/bmrMvc-g58132/demo2 directory

--- Projet/bmrMvc-g58132/demo2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demo2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens controlerView to javafx.fxml;
-    exports controlerView;
-}
